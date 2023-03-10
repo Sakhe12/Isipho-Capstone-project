ee6e4280a2cd0d77018b895ebc69504e671cb3c5
Sakhe12 <sakhemzolisa@gmail.com>
2023-03-10 16:54:27 +0200

    first push

---
+++ isipho_front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <navbar></navbar>
+  <router-view/>
+</template>
+
+<script>
+import navbar from './components/Navbar.vue'
+export default {
+  components : {
+    navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Raleway', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
---
+++ isipho_front/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template lang="">
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand logo" href="#">Isipho: A Gift From Africa</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
+                  <li class="nav-item">
+                    <router-link class="txt" to="/">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="txt" to="/products">Products</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="txt" to="/admin">admin</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="txt" to="/sign_in">Sign In / Register</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="txt" to="/userProfile">Cart</router-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+<script>
+export default {
+    name: 'navbar_bar'
+}
+</script>
+<style >
+    .txt {
+        text-decoration: none;
+    }
+    .logo {
+        font-family: 'Pacifico', cursive;
+    }
+</style>
